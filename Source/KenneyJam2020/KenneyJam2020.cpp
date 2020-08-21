361fb503d2c712a0f1a409fe84becbc919603fb5
Ji_Rath <garethaguilar@gmail.com>
2020-08-21 13:37:49 -0500

    Add initial files Add gitignore file

---
+++ Source/KenneyJam2020/KenneyJam2020.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KenneyJam2020.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KenneyJam2020, "KenneyJam2020" );
