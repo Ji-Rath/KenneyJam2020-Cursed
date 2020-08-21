3f6948a5bd78c79c200837c1e545b75e5ea57316
Ji_Rath <garethaguilar@gmail.com>
2020-08-21 18:26:29 -0500

    Implement movement, jumping, and control rotation

--- Source/KenneyJam2020/PlayerBase.cpp
+++ Source/KenneyJam2020/PlayerBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PlayerBase.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 APlayerBase::APlayerBase()
@@ -29,6 +30,29 @@ void APlayerBase::Tick(float DeltaTime)
 void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerBase::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerBase::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &APlayerBase::Interact);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+}
+
+void APlayerBase::MoveForward(float AxisValue)
+{
+	AddMovementInput(UKismetMathLibrary::GetForwardVector(GetControlRotation()) * 1000.f, AxisValue);
+}
+
+void APlayerBase::MoveRight(float AxisValue)
+{
+	AddMovementInput(UKismetMathLibrary::GetRightVector(GetControlRotation()) * 1000.f, AxisValue);
+}
+
+void APlayerBase::Interact()
+{
 
 }
 
